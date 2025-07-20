ed479761dbf63fca6b1e86066f7e3082d7d37d6d
Isuru Madhuwantha <prasadnwip@gmail.com>
2025-07-20 07:32:15 +0530

    initial project setup

--- scripts/postRun.ts
+++
@@ -1,7 +0,0 @@
-import { sendEmail } from '../utils/email';
-import { sendSlackNotification } from '../utils/slack';
-
-(async () => {
-  await sendEmail();
-  await sendSlackNotification();
-})();
--- tests/todo.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Add a todo item', async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc/');
-  await page.getByPlaceholder('What needs to be done?').fill('Write Playwright Tests');
-  await page.getByPlaceholder('What needs to be done?').press('Enter');
-  await expect(page.locator('.todo-list li')).toHaveText(['Write Playwright Tests']);
-});
